32187402a37c3e10a3218085409a407fcff53a66
Pol Henarejos <pol.henarejos@cttc.es>
2025-12-16 23:19:26 +0100

    Fix with previous release
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- picoboot/__init__.py
+++ picoboot/__init__.py
@@ -1,3 +1,3 @@
 from ._version import __version__
-from .picoboot import PicoBoot, Model
+from .picoboot import PicoBoot, Platform
 from .core.exceptions import PicoBootError, PicoBootNotFoundError, PicoBootInvalidStateError
